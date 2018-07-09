9d77fd684c5118a49bc8b2988c5d249b766964b7
Oleksii <salo1d2323@gmail.com>
2018-07-10 01:17:24 +0300

    admin log in\out

--- app.py
+++ app.py
@@ -15,3 +15,20 @@ def home():
         return render_template('log_in.html')
     else:
         return render_template('logged_in.html')
+
+@app.route('/login', methods=['POST'])
+def do_login():
+ 
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
